ca98387cc2358cf8916866f50dc92261259132c6
Илья Еремин <partissanei@gmail.com>
2016-01-25 02:07:48 +0300

    variable -> static field

--- app/src/main/java/ru/ilyaeremin/vkdialogs/utils/Randoms.java
+++ app/src/main/java/ru/ilyaeremin/vkdialogs/utils/Randoms.java
@@ -18,14 +18,13 @@ public class Randoms {
     }
 
     // Implementing Fisher–Yates shuffle
+    private static Random rnd = new Random();
+
     public static void shuffleArray(int[] ar)
     {
-        // If running on Java 6 or older, use `new Random()` on RHS here
-        Random rnd = new Random();
         for (int i = ar.length - 1; i > 0; i--)
         {
             int index = rnd.nextInt(i + 1);
-            // Simple swap
             int a = ar[index];
             ar[index] = ar[i];
             ar[i] = a;
